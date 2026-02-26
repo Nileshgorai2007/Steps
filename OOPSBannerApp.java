2115834742161987656afe578c2edb7ae68c2abd
Nilesh_dev <nileshgorai2007@gmail.com>
2026-02-26 14:39:59 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -8,7 +8,6 @@
 public class uc5 {
     public static void main(String[] args) {
 
-        // Define and populate the array at declaration
         String[] lines = {
             String.join("   ", "   ***  ", "   ***  ", "  *****  ", "  *****  "),
             String.join("   ", " **   **", " **   **", "  **   **", " **    "),
@@ -19,7 +18,6 @@ public class uc5 {
             String.join("   ", "  ***    ", "  ***    ", "**      ", "  ***  ")
         };
 
-        // Use a for-each loop to iterate through the array and print each line
         for (String line : lines) {
             System.out.println(line);
         }
